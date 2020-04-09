d4f1df0e281033a9d901d8f432defbff573402ed
Raul Guedes <rg.oliveira08@gmail.com>
2020-04-09 06:43:58 -0300

    Primeira implementação do css

--- index.php
+++ index.php
@@ -3,33 +3,35 @@
     <head>
         <meta charset="UTF-8">
         <title>Formulário</title>
-        <link rel="stylesheet" type="text/css" href="CSS_File">
+        <link rel="stylesheet" type="text/css" href="Estilo.css">
     </head>
     
     <body>
-        <div id="MainDiv">
+        <h2> <u>Cadastro de Produtos</u> </h2> <br>
+        <div id="mainDiv">
             <form method="GET" action="store.php" enctype="multipart/form-data">
                 
-                <label>Nome do Produto:</label>
-                <input type="text" name="txtNome"> <br>
+                <label>Nome do Produto:</label> <br>
+                <input type="text" name="txtNome" id="txtNome"> <br>
                 
-                <label>Preço:</label>
+                <label>Preço:</label> <br>
                 <input type="number" name="txtValor"> <br>
                 
-                <label>Categoria:</label>
+                <label>Categoria:</label> <br>
                 <select name="Categoria">
                     <option value="0">Selecione...</option>
-                </select>
-                
-                <label>Quantidade:</label>
-                <input type="number" name="txtQuantidade"> <br>
+                </select><br>
                 
                 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
                 <hr>
-                <label>Data de validade:</label>
+                
+                <label>Quantidade:</label> <br>
+                <input type="number" name="txtQuantidade"> <br>
+                
+                <label>Data de validade:</label> <br>
                 <input type="date" name="validade"> <br>
                 
-                <label>Foto do Produto:</label>
+                <label>Foto do Produto:</label> <br>
                 <input type="file" name="fotoProduto">
                 
             </form>
